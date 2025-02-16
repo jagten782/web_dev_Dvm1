9161819f356569686334168d53e9930521f2beca
Aditya Singh <vidhu3968@gmail.com>
2025-02-16 15:35:43 +0530

    Update settingsProd.py

--- web_dev/web_dev/settingsProd.py
+++ web_dev/web_dev/settingsProd.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'albus_data',
-        'USER': 'aditya',
+        'USER': 'albus',
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
